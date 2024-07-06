944d574009fba636ec92d5b75e8323df819e5ae1
DovydasT <tamasauskas.dovydas@gmail.com>
2024-07-06 12:01:32 +0300

    added match ending result action redux

--- src/components/Games/Match.tsx
+++ src/components/Games/Match.tsx
@@ -1,10 +1,40 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { recordBoard } from "../../reducers/rootReducer";
+
 const Match = (props: any) => {
   const { team1, team2 } = props;
 
+  const [score1, setScore1] = useState<string>();
+  const [score2, setScore2] = useState<string>();
+  const dispatch = useDispatch();
+
+  const onClick = () => {
+    console.log(score1, score2);
+    dispatch(recordBoard({ team1, team2, score1, score2 }));
+  };
+
+  const onChange = (setTeamScrore: { (): void; (): void }) => {
+    setTeamScrore();
+  };
+
   return (
     <div>
-      {team1} <input type="text" name={team1} style={{ width: "15px" }} />:
-      <input type="text" name={team2} style={{ width: "15px" }} /> {team2}
+      {team1}{" "}
+      <input
+        type="text"
+        name={team1}
+        style={{ width: "15px" }}
+        onChange={(e) => onChange(() => setScore1(e.target.value))}
+      />
+      :
+      <input
+        type="text"
+        name={team2}
+        style={{ width: "15px" }}
+        onChange={(e) => onChange(() => setScore2(e.target.value))}
+      />{" "}
+      {team2} <button onClick={onClick}>submit</button>
     </div>
   );
 };
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -21,9 +21,30 @@ export const rootStore = createSlice({
         };
       }
     },
+    recordBoard: (state, action) => {
+      const { team1, team2, score1, score2 } = action.payload;
+
+      if (score1 > score2) {
+        state.leaderBoard[team1].points += 3;
+        state.leaderBoard[team1].wins += 1;
+        state.leaderBoard[team2].losses += 1;
+      } else if (score1 < score2) {
+        state.leaderBoard[team2].points += 3;
+        state.leaderBoard[team2].wins += 1;
+        state.leaderBoard[team1].losses += 1;
+      } else {
+        state.leaderBoard[team1].points += 1;
+        state.leaderBoard[team2].points += 1;
+        state.leaderBoard[team1].draws += 1;
+        state.leaderBoard[team2].draws += 1;
+      }
+
+      state.leaderBoard[team1].gamesPlayed += 1;
+      state.leaderBoard[team2].gamesPlayed += 1;
+    },
   },
 });
 
-export const { addTeam } = rootStore.actions;
+export const { addTeam, recordBoard } = rootStore.actions;
 
 export default rootStore.reducer;
