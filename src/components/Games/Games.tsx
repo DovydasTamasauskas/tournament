dc2918ca09c8e5e0c2fed5b7c896794d5a8b441e
DovydasT <tamasauskas.dovydas@gmail.com>
2024-07-06 11:13:13 +0300

    clean up

--- src/components/Games/Games.tsx
+++ src/components/Games/Games.tsx
@@ -3,15 +3,6 @@ import { useSelector } from "react-redux";
 
 const Games = () => {
   const teams = useSelector((state: any) => state.teams);
-  console.log(
-    teams
-      .flatMap((team1: any, index1: number) => {
-        return teams.map((team2: any, index2: number) => {
-          return index1 < index2 ? { team1, team2 } : null;
-        });
-      })
-      .filter((teams: any) => teams)
-  );
   return (
     <>
       {teams &&
