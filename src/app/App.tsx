b4ca3ea7b169a47ab1bf1db74e34afa194c0099c
DovydasT <tamasauskas.dovydas@gmail.com>
2024-07-10 09:31:09 +0300

    clean up

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,20 +3,18 @@ import Table from "../components/Table/Table";
 import AddTeam from "../components/AddTeam/AddTeam";
 import Games from "../components/Games/Games";
 
-function App() {
-  return (
-    <div className="container">
-      <div className="add-team">
-        <AddTeam />
-      </div>
-      <div className="leader-board">
-        <Table />
-      </div>
-      <div className="match-making">
-        <Games />
-      </div>
+const App = () => (
+  <div className="container">
+    <div className="add-team">
+      <AddTeam />
     </div>
-  );
-}
+    <div className="leader-board">
+      <Table />
+    </div>
+    <div className="match-making">
+      <Games />
+    </div>
+  </div>
+);
 
 export default App;
